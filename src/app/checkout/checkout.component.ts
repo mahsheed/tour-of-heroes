f5979c09bcaf4b2901fa13e9c03613a0472b92d3
Mahsheed Parham <melanie.parham@gmail.com>
2018-04-05 23:52:54 -0400

    everything after at start of step 2 make one form

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,21 @@
+import { NgModule }       from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule }  from '@angular/platform-browser';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ]
+})
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
+
 export class CheckoutComponent implements OnInit {
 
   constructor() { }
