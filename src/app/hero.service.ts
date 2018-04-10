27f6fc7649857c6d60f6b851fcc7bbe6bfc4f2dd
Mahsheed Parham <melanie.parham@gmail.com>
2018-04-10 18:22:32 -0400

    did some formatting to app.component.css

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-
 import { MessageService } from './message.service';
 
 @Injectable()
