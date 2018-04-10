37512d2ac266235bceaa4a0ed3da9628c1ce36f0
Mahsheed Parham <melanie.parham@gmail.com>
2018-04-10 17:09:15 -0400

    removed mock-heroes.ts and modified hero.service.ts

--- src/app/mock-heroes.ts
+++
@@ -1,9 +0,0 @@
-import { Hero } from './hero';
-
-export const HEROES: Hero[] = [
-  { id: 1, name: 'Blackest Black Mascara' },
-  { id: 2, name: 'Violet Mascara' },
-  { id: 3, name: 'Evergreen Mascara' },
-  { id: 4, name: 'Cocoa Mascara' },
-];
-
